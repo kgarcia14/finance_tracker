20cfb99d2ac72efe5ace5bf0eb567264fa6055ff
Kurtis Garcia <kurtisgarcia14@gmail.com>
2022-04-12 15:17:01 -0400

    added functionality to switch back and forth from add expense and add deposit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const expenseBtn = document.querySelector('.expense-btn');
+const depositBtn = document.querySelector('.deposit-btn');
+const modalContentExpense = document.querySelector('.modal-content-expense');
+const modalContentDeposit = document.querySelector('.modal-content-deposit');
+
+expenseBtn.addEventListener('click', () => {
+    expenseBtn.classList.add('active-expense-btn');
+    depositBtn.classList.remove('active-deposit-btn');
+    expenseBtn.classList.remove('inactive-expense-btn');
+    modalContentExpense.classList.remove('hidden');
+    modalContentDeposit.classList.add('hidden');
+});
+
+depositBtn.addEventListener('click', () => {
+    depositBtn.classList.add('active-deposit-btn');
+    expenseBtn.classList.remove('active-expense-btn');
+    expenseBtn.classList.add('inactive-expense-btn');
+    modalContentExpense.classList.add('hidden');
+    modalContentDeposit.classList.remove('hidden');
+});
