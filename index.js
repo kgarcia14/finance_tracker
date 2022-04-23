56604ab28d18d0b52564598c5f96b4690d1f9d51
Kurtis Garcia <kurtisgarcia14@gmail.com>
2022-04-23 10:17:26 -0400

    added cancel button to close modal

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 'use strict';
 
+const modal = document.querySelector('.modal-overlay');
+const addTransactionBtn = document.querySelector('.add-transaction-btn');
+const cancelBtn = document.querySelectorAll('.cancel-btn');
 const expenseBtn = document.querySelector('.expense-btn');
 const depositBtn = document.querySelector('.deposit-btn');
 const modalContentExpense = document.querySelector('.modal-content-expense');
 const modalContentDeposit = document.querySelector('.modal-content-deposit');
+
+addTransactionBtn.addEventListener('click', () => {
+
+});
+
+cancelBtn.forEach((cancelButtons) => {
+    cancelButtons.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('cancel clickeddd')
+        modal.classList.add('hidden');
+    })
+});
 
 expenseBtn.addEventListener('click', () => {
     expenseBtn.classList.add('active-expense-btn');
