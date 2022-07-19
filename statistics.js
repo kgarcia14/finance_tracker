fd51dfd8bc75e717452df9eadaf04645e1b834cc
Kurtis Garcia <kurtisgarcia14@gmail.com>
2022-07-19 15:30:30 -0400

    implemented chart.js, still need to configure

--- statistics.js
+++ statistics.js
@@ -1,2 +1,35 @@
 'use strict';
 
+const labels = [
+    'January',
+    'February',
+    
+];
+
+const data = {
+    labels: [
+        'Expenses',
+        'Income',
+        'Savings',
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [60, 50, 100],
+      backgroundColor: [
+        '#6488E5',
+        '#F9BE7D',
+        '#E56372',
+      ],
+      hoverOffset: 4
+    }]
+};
+
+const config = {
+    type: 'doughnut',
+    data: data,
+};
+
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
